7c3c130e0413d28d6ce93b4e8ff2bd8e8726d296
siromiji <mnmn82018282@gmail.com>
2025-09-08 17:58:13 +0900

    feat: detail/count 로직 및 라우터 정리

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,7 +1,30 @@
+import React, {useState} from 'react';
+import { addCnt,minusCnt,changeCnt } from '../store/slices/detail.js';
+import './Detail.css'
+import { useDispatch,useSelector} from 'react-redux';
+
 function Detail() {
+  const dispatch = useDispatch();
+  const cnt = useSelector(state => state.detail.cnt);
+  const [inputNum , setInputNum] = useState(0);
+
+  const convertAndSetNumber = (e) =>{
+    if(!isNaN(e.target.value)){
+      setInputNum(parseInt(e.target.value));
+    }else{
+      console.error('숫자아님');
+    }
+  }
+   //action 호출 방법 
+
   return(
-    <>
-      <h2>상세 페이지</h2>
+     <>
+      <h1>상세페이지</h1>
+      <p>{cnt}</p>
+      <button onClick={() =>{dispatch(addCnt())}}>+</button>
+      <button onClick={() =>{dispatch(minusCnt())}}>-</button>
+      <input type="number" onChange={convertAndSetNumber}/>
+      <button type="button" onClick={()=> dispatch(changeCnt(inputNum))}>입력</button>
     </>
   )
 }
