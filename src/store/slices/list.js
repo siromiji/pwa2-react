9ee96289ec16e20da10d57aa4065e45b8c046d03
siromiji <mnmn82018282@gmail.com>
2025-09-09 16:01:00 +0900

    250909 reducs thunk

--- src/store/slices/list.js
+++ src/store/slices/list.js
@@ -1,10 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getList , getTest} from "../thunks/listThunk.js";
 
 // 'list ' 상태관리 slice 생성 및 설정
 const list = createSlice({
   name: 'list', //slice 명
   initialState: { //상태 관리할 state를 정의하는 영역
     cnt : 0,
+    list: null,
+    loading: false,
   },
   reducers: { //state의 상태를 변화시키는 actions를 정의하는 영역
     addCnt(state){
@@ -18,13 +21,58 @@ const list = createSlice({
       //action : 외부에서 전달 된 값을 담는 파라미터
       // > action.payload : 전달 된 값에 접근할 수 있는 프로퍼티
       state.cnt = action.payload;
-    }
+    },
+    clearList(state){
+      state.list=null;
+    },
+  },
+  extraReducers: builder =>{
+    builder
+      // .addCase(getList.pending, (state)=>{
+      //   // 'getlist' Thunk의 처리가 대기중일 때의 처리를 작성 
+      //   state.loading = true;
 
+      // })
+      .addCase(getList.fulfilled,(state, action) => {
+        //'getList' Thunk의 처리가 성공 일 때의 처리를 작성
+        state.list = action.payload;
+        state.loading = false;
+
+      })
+      // .addCase(getList.rejected,(state) => {
+      //   //'getList' Thunk의 처리가 실패 일 때의 처리를 작성
+      //   state.loading = false;
+      // })
+      .addCase(getTest.fulfilled,(state, action) => {
+        //'getList' Thunk의 처리가 성공 일 때의 처리를 작성
+        state.list = action.payload;
+        state.loading = true;
+
+      })
+      // .addCase(getTest.fulfilled,(state, action) => {
+      //   //'getList' Thunk의 처리가 실패 일 때의 처리를 작성
+        
+      //   state.loading = false;
+
+      // })
+     .addMatcher(
+      action => action.type.endsWith('/pending'),
+      state => {
+        state.loading = true;
+      }
+     )
+     .addMatcher(
+      action => action.type.endsWith('/rejected'),
+        state => {
+        state.loading = false;
+      }
+     )
+      ;
   }
 });
 
 //정의한 Actions 내보내기 
-export const {addCnt, changeCnt, minusCnt} = list.actions;
+export const {addCnt, changeCnt, minusCnt, clearList} = list.actions;
 
 // Reducer 내보내기
 export default list.reducer;
